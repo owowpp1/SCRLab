e9082dc2a0033dea23d30d54beaac0b3685242a8
owowpp1 <62721108+owowpp1@users.noreply.github.com>
2021-01-06 23:55:12 +0100

    zad 1-4

---
+++ Lab7/Hello.c
@@ -0,0 +1,28 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define THREADY	8
+
+void *Wypisz(void *id)
+{
+   long idlong;
+   idlong = (long)id;
+   printf("Hello SCR. Written by thread ID %d!\n", idlong+1);
+   pthread_exit(NULL);
+}
+
+int main()
+{
+    pthread_t watki[THREADY];
+    for(long i=0; i<THREADY; i++){
+        printf("Created thread %d\n", i+1);
+        if(pthread_create(&watki[i], NULL, Wypisz, (void *)i)){
+            printf("Błąd w pthread_create() dla wątku %d\n", i+1);
+            return -1;
+            }
+//        sleep(1);
+//        pthread_join(watki[i], NULL);
+    }
+   pthread_exit(NULL);
+}
